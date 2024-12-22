8cc0b9e80c32447b758ab561fd5f0e5bdfbd6f6a
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-22 10:54:13 -0300

    feat: edit, detail, delete para class Transaccion

--- src/gestion/urls.py
+++ src/gestion/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (index, about, usuario_create,usuario_list,transaccion_create,transaccion_list,informe_create,informe_list)
+from .views import (index, about, usuario_create,usuario_list,transaccion_create,transaccion_list, transaccion_update, transaccion_detail, transaccion_delete, informe_create,informe_list)
 
 app_name = "gestion"
 
@@ -10,6 +10,9 @@ urlpatterns = [
     path("usuario/create/", usuario_create, name="usuario_create"),
     path("transaccion/list/", transaccion_list, name="transaccion_list"),
     path("transaccion/create/", transaccion_create, name="transaccion_create"),
+    path("transaccion/update/<int:pk>", transaccion_update, name="transaccion_update"),
+    path("transaccion/detail/<int:pk>", transaccion_detail, name="transaccion_detail"),
+    path("transaccion/delete/<int:pk>", transaccion_delete, name="transaccion_delete"),
     path("informe/list/", informe_list, name="informe_list"),
     path("informe/create/", informe_create, name="informe_create"),
 ]
--- src/gestion/views.py
+++ src/gestion/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .forms import UsuarioForm, TransaccionForm, InformeForm
@@ -41,6 +42,32 @@ def transaccion_create(request: HttpRequest) -> HttpResponse:
             form.save()
             return redirect("gestion:transaccion_list")
     return render(request, "gestion/transaccion_form.html", {"form": form})
+
+def transaccion_update(request: HttpRequest, pk:int) -> HttpResponse:
+    query = Transaccion.objects.get(id=pk)
+    if request.method == "GET":
+        form = TransaccionForm(instance=query)
+    if request.method == "POST":
+        form = TransaccionForm(request.POST, instance=query)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Transacción actualizada correctamente')
+            return redirect("gestion:transaccion_list")
+    return render(request, "gestion/transaccion_form.html", {"form": form})
+
+def transaccion_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    query = Transaccion.objects.get(id=pk)
+    return render(request, 'gestion/transaccion_detail.html', {'object': query})
+
+def transaccion_delete(request: HttpRequest, pk:int) -> HttpResponse:
+    query = Transaccion.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        messages.success(request, 'Transacción eliminada')
+        return redirect("gestion:transaccion_list")
+    return render(request, "gestion/transaccion_confirm_delete.html", {'object': query})
+
+
 
 
 def informe_list(request: HttpRequest) -> HttpResponse:
