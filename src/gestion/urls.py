a2034b8690094b814857ec91d5de3bf1b879b97f
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-22 12:14:45 -0300

    feat: Views como clases Transacción

--- src/gestion/urls.py
+++ src/gestion/urls.py
@@ -6,14 +6,19 @@ app_name = "gestion"
 
 urlpatterns = [
     path('', index, name="index"),
-    path('about/', about, name="about"),
+    path('about/', about, name="about"),]
+
+urlpatterns += [
     path("usuario/list/", usuario.usuario_list, name="usuario_list"),
-    path("usuario/create/", usuario.usuario_create, name="usuario_create"),
-    path("transaccion/list/", transaccion.transaccion_list, name="transaccion_list"),
-    path("transaccion/create/", transaccion.transaccion_create, name="transaccion_create"),
-    path("transaccion/update/<int:pk>", transaccion.transaccion_update, name="transaccion_update"),
-    path("transaccion/detail/<int:pk>", transaccion.transaccion_detail, name="transaccion_detail"),
-    path("transaccion/delete/<int:pk>", transaccion.transaccion_delete, name="transaccion_delete"),
+    path("usuario/create/", usuario.usuario_create, name="usuario_create"),]
+
+urlpatterns += [
+    path("transaccion/list/", transaccion.TransaccionListView.as_view(), name="transaccion_list"),
+    path("transaccion/create/", transaccion.TransaccionCreateView.as_view(), name="transaccion_create"),
+    path("transaccion/update/<int:pk>", transaccion.TransaccionUpdateView.as_view(), name="transaccion_update"),
+    path("transaccion/detail/<int:pk>", transaccion.TransaccionDetailView.as_view(), name="transaccion_detail"),
+    path("transaccion/delete/<int:pk>", transaccion.TransaccionDeleteView.as_view(), name="transaccion_delete"),]
+
+urlpatterns += [
     path("informe/list/", informe.informe_list, name="informe_list"),
-    path("informe/create/", informe.informe_create, name="informe_create"),
-]
+    path("informe/create/", informe.informe_create, name="informe_create"),]
--- src/gestion/views_models/transaccion.py
+++ src/gestion/views_models/transaccion.py
@@ -1,45 +1,23 @@
-from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.http import HttpRequest, HttpResponse
 from ..forms import TransaccionForm
 from ..models import Transaccion
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.urls import reverse_lazy
 
-def transaccion_list(request: HttpRequest) -> HttpResponse:
-    query = Transaccion.objects.all()
-    context = {"object_list": query}
-    return render(request, "gestion/transaccion_list.html", context)
+class TransaccionListView(ListView):
+    model = Transaccion
 
+class TransaccionCreateView(CreateView):
+    model = Transaccion
+    form_class = TransaccionForm
+    success_url = reverse_lazy("gestion:transaccion_list")
 
-def transaccion_create(request: HttpRequest) -> HttpResponse:
-    if request.method == "GET":
-        form = TransaccionForm()
-    if request.method == "POST":
-        form = TransaccionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("gestion:transaccion_list")
-    return render(request, "gestion/transaccion_form.html", {"form": form})
+class TransaccionUpdateView(UpdateView):
+    model = Transaccion
+    form_class = TransaccionForm
+    success_url = reverse_lazy("gestion:transaccion_list")
 
-def transaccion_update(request: HttpRequest, pk:int) -> HttpResponse:
-    query = Transaccion.objects.get(id=pk)
-    if request.method == "GET":
-        form = TransaccionForm(instance=query)
-    if request.method == "POST":
-        form = TransaccionForm(request.POST, instance=query)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Transacción actualizada correctamente')
-            return redirect("gestion:transaccion_list")
-    return render(request, "gestion/transaccion_form.html", {"form": form})
+class TransaccionDetailView(DetailView):
+    model = Transaccion
 
-def transaccion_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    query = Transaccion.objects.get(id=pk)
-    return render(request, 'gestion/transaccion_detail.html', {'object': query})
-
-def transaccion_delete(request: HttpRequest, pk:int) -> HttpResponse:
-    query = Transaccion.objects.get(id=pk)
-    if request.method == "POST":
-        query.delete()
-        messages.success(request, 'Transacción eliminada')
-        return redirect("gestion:transaccion_list")
-    return render(request, "gestion/transaccion_confirm_delete.html", {'object': query})
+class TransaccionDeleteView(DeleteView):
+    model = Transaccion
