2987df7aa4dd7b504e67c30df987ea4b8c8be58d
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-22 08:59:48 -0300

    feat: Correcciones entrega 3

--- src/gestion/models.py
+++ src/gestion/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+TIPOS_TRANSACCIONES =[("I","Ingreso"),("G","Gasto")]
+
+OPERACIONES = [("A","Alimentos"),
+             ("AS","Alquiler y Servicios"),
+             ("S","Shopping"),
+             ("R","Regalos"),
+             ("E","Entretenimiento"),
+             ("V","Viajes"),
+             ("S","Sueldo"),
+             ("O","Otros")]
+
+TIPOS_INFORME = [
+    ('ING', 'Ingreso'),
+    ('GAS', 'Gasto'),
+    ('BAL', 'Balance General'),
+]
+
+PERIODOS = [
+    ('MENSUAL', 'Mensual'),
+    ('TRIMESTRAL', 'Trimestral'),
+    ('ANUAL', 'Anual'),
+]
 # def normalizar_texto(texto: str) -> str:
 #     """Transforma el texto a una forma normalizada NFD
 #     (Normalization Form Decomposition).
@@ -23,18 +45,9 @@ class Usuario(models.Model):
 
 class Transaccion(models.Model):
     nombre = models.ForeignKey(Usuario,on_delete=models.CASCADE)
-    tipo_transaccion =[("I","Ingreso"),("G","Gasto")]
-    tipo = models.CharField(max_length=1,choices=tipo_transaccion)
+    tipo = models.CharField(max_length=1,choices=TIPOS_TRANSACCIONES)
     monto = models.DecimalField(max_digits=10,decimal_places=2)
-    categorias = [("A","Alimentos"),
-                 ("AS","Alquiler y Servicios"),
-                 ("S","Shopping"),
-                 ("R","Regalos"),
-                 ("E","Entretenimiento"),
-                 ("V","Viajes"),
-                 ("S","Sueldo"),
-                 ("O","Otros")]
-    categoria = models.CharField(max_length=50,choices=categorias,default="O")
+    categoria = models.CharField(max_length=50,choices=OPERACIONES,default="O")
     descripcion = models.TextField(max_length=50,null=True,blank=True)
     fecha = models.DateField(null=True,blank=True)
 
@@ -42,19 +55,8 @@ class Informe(models.Model):
     nombre = models.ForeignKey(Usuario,on_delete=models.CASCADE,null=True)
     año = models.PositiveIntegerField()
     informe = models.CharField(max_length=25)
-    categorias = [
-        ('ING', 'Ingreso'),
-        ('GAS', 'Gasto'),
-        ('BAL', 'Balance General'),
-    ]
-    categoria = models.CharField(max_length=3,choices=categorias,default='BAL')
-    periodos = [
-        ('MENSUAL', 'Mensual'),
-        ('TRIMESTRAL', 'Trimestral'),
-        ('ANUAL', 'Anual'),
-    ]
-    periodo = models.CharField(max_length=10,choices=periodos,default='ANUAL')
-
+    categoria = models.CharField(max_length=3,choices=TIPOS_INFORME,default='BAL')
+    periodo = models.CharField(max_length=10,choices=PERIODOS,default='ANUAL')
 
     class Meta():
         constraints = [models.UniqueConstraint(fields=["año","categoria","periodo"], name="unico_informe_por_año")]
