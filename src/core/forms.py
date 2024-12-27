0680fd4c5ecf0433c19e3907ebf37506f322f3f3
Aylen Poschner <aylenposchner@hotmail.com>
2024-12-27 19:38:36 -0300

    feat: Modificaciones register

--- src/core/forms.py
+++ src/core/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,7 +7,14 @@ class CustomAuthenticationForm(AuthenticationForm):
         model = AuthenticationForm
         fields = ['username','password']
 
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "password1", "password2"]
+        help_texts = {'username': ''}
+    
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
